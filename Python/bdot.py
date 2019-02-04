7de17adb2c34ded6c1a32813fc8ad039a23c1bf3
Neethu_nr <neethurenjith0@gmail.com>
2019-02-04 00:35:55 -0800

    B plot

--- Python/bdot.py
+++
@@ -1,7 +0,0 @@
-import pickle
-from igrffx import igrffx
-
-pickle.dump(orbital_position_data,open("adcs\Parameters\orbital_position.txt","rb"))
-x=orbital_position_data["x"]
-time=orbital_position_data["t"]
-print(x[:3,:3])
