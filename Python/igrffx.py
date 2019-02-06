ec2aef7f81c255715fa0f0b86b26ef3bce22e144
kevintracy7 <43561408+kevintracy7@users.noreply.github.com>
2019-02-06 15:54:30 -0800

    Update igrffx.py

--- Python/igrffx.py
+++ Python/igrffx.py
@@ -1,4 +1,5 @@
-def igrffx(eci_vec,year,month,day,hour,minute,second,microsecond):
+def igrffx(eci_vec,time):
+	#import igrf12
 	import numpy
 	import pyIGRF
 	import pymap3d
@@ -8,12 +9,12 @@ def igrffx(eci_vec,year,month,day,hour,minute,second,microsecond):
 	import datetime
 	from datetime import datetime
 	import astropy
-	#eci_vec is a xyz vector in ECI in KM
+	#eci_vec is a xyz vector in ECI 
 	#output B_ECI is a 3 item array in units of nT
 	
 	#get time 
 	#datetime(year, month, day, hour, minute, second, microsecond)
-	time = datetime(year, month, day, hour, minute, second, microsecond)
+	#time = datetime(year, month, day, hour, minute, second, microsecond)
 
 	#get our lat long and alt from ECI 
 	geod = pymap3d.eci2geodetic(1000*eci_vec, time, useastropy=True)
@@ -23,10 +24,15 @@ def igrffx(eci_vec,year,month,day,hour,minute,second,microsecond):
 	altitude = geod[2] #meters
 
 	#call igrf to get b vector in NED
-	#b in NED in nT
+	#mag = igrf12.igrf('2019-01-12', glat=latitude, glon=longitude, alt_km=altitude/1000)
 	b = pyIGRF.igrf_value(latitude, longitude, altitude/1000, 2019)
+	#pull NED components out of xarray.dataset 
+	#b_north = mag.north.values[0]
+	#b_east = mag.east.values[0]
+	#b_down = mag.down.values[0]
 
 	#combine NED components back into an array
+	#NED = numpy.array([b_north,b_east,b_down])
 	NED = b[3:6]
 
 	#convert from NED to ECEF
@@ -36,6 +42,7 @@ def igrffx(eci_vec,year,month,day,hour,minute,second,microsecond):
 	B_ECI = pymap3d.ecef2eci(ECEF, time, useastropy=True)
 
 	return B_ECI
+
 
 
 
