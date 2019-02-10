d98c4a7b6668fb5019ee50d5bd6e8184f2e47155
Neethu-nr <46496337+Neethu-nr@users.noreply.github.com>
2019-02-10 13:16:35 -0800

    Add files via upload

--- Python/B.py
+++ Python/B.py
@@ -8,7 +8,9 @@ import julian
 import datetime
 from subroutines import *
 from HSTdynamics import HSTdynamics
+from HSTdynamics2 import HSTdynamics2
 from igrffx import igrffx
+import pdb
 plt.close('all')
 
 ## Initialize and Save Constant Parameters
@@ -23,39 +25,45 @@ mu = 3.986e5            # Earth Standard Gravitational Parameter (km^3/s^2)
 T = 2*np.pi*np.sqrt((a**3)/mu) # Orbital Period (s)
 
 # Mass Properties, Principle Axes of Inertia
-J11 = 28525.53
-J22 = 174815.86
-J33 = 181630.81
+J11 = .01/6
+J22 = .01/6
+J33 = .01/6
 J=np.array([J11,J22,J33])
 J = np.diag(J) # [x-axis z-axis y-axis]
 
-# Compute Perturbed Attitude Dynamics
-tol = 1e-6
-
 # Convert to Earth-Centered Inertial Frame Coordinates
 r_eci, v_eci = OE2ECI(a, e, i, RAAN, w, anom, mu)
-torque = np.array([0,0,0]).T
-q = np.array([0,0,0,1]).T # Identity Quaternion
-om = np.array([0.25,0.25,2.5]).T # rad/s
+torque = np.zeros((1,3)).T
+q = np.zeros((1,4)).T # Identity Quaternion
+q[3]=1
+om = 0.25*np.ones((1,3)).T # rad/s
 tspan=[0]
 B=np.zeros((1,3))
-r_total = np.array([])
-v_total = np.array([])
-om_total = np.array([])
-q_total = np.array([])
-
+r_total = np.zeros((1,3))
+v_total = np.zeros((1,3))
+om_total = np.zeros((1,3))
+q_total = np.zeros((1,4))
+Torque=np.zeros((1,3))
+Tspan=[]
+m_max=5*np.ones((1,3))
 i=1
-for i in range (1,10):
-	tspan = np.arange(tspan[0], 0.1*T*i, 10)
-	
-	rv_eci0 = np.append(r_eci[-1], v_eci[-1])
-	rv_eci = odeint(HSTdynamics, rv_eci0, tspan, args=(mu,J,torque))
-	omq0 = np.append(om[-1],q[-1])
-	omq = odeint(HSTdynamics, omq0, tspan, args=(mu,J,torque))
-	r_total = np.append(r_total,rv_eci[:,:3])
-	v_total = np.append(v_total,rv_eci[:,3:])
-	om_total = np.append(om_total,omq_eci[:,:3])
-	q_total = np.append(q_total,omq_eci[:,3:])
+for i in range (1,3):
+	print (i)
+	tspan = np.arange(tspan[0], 0.05*T*i, 10)
+	if i==1:
+		rv_eci0 = np.append(r_eci, v_eci)
+		omq0 = np.append(om,q)
+	else:
+		rv_eci0 = np.append(r_total[-1,:], v_total[-1,:])
+		omq0 = np.append(om_total[-1,:],q_total[-1,:])
+
+	rv_eci = odeint(HSTdynamics2, rv_eci0, tspan, args=(mu,J,0))	
+	omq = odeint(HSTdynamics2, omq0, tspan, args=(mu,J,torque.reshape(1,3)))
+	r_total = np.concatenate((r_total,rv_eci[:,0:3]),axis=0)
+	v_total = np.concatenate((v_total,rv_eci[:,3:6]),axis=0)
+	om_total = np.concatenate((om_total,omq[:,0:3]),axis=0)
+	q_total = np.concatenate((q_total,omq[:,3:7]),axis=0)
+	Tspan=np.concatenate((Tspan,tspan),axis=0)
 
 	q_current = omq[-1,3:7]
 	om_current=omq[-1,:3]
@@ -64,25 +72,24 @@ for i in range (1,10):
 	mjd = 54372.78
 	dt = julian.from_jd(mjd, fmt='mjd')
 	newtime = dt + datetime.timedelta(seconds=tspan[j])
-	B=(igrffx(rv_eci[j][0:3],newtime).T)
+	B_eci=(igrffx(rv_eci[j][0:3],newtime).T)
 
-	u_max=10
-	B = np.dot(q2rot(q_current),B)
-	B_moment_value = u_max*np.tanh(np.dot(om.T,B))
-	B_moment_direction = np.cross(om,B) 
-	B_moment_direction = B_moment_direction/np.linalg.norm(B_moment_direction)
-	B_moment = B_moment_value * B_moment_direction
-	torque = np.cross(B_moment,B)
-	torque=np.matmul(np.linalg.inv(J),torque)
-	torque = np.array([0,0,0]).T
+ 
+	B_body = np.dot(q2rot(q_current),B_eci)
+	B_dot = -np.cross(om_current,B_body)
+	m_value = -np.multiply(m_max,np.sign(B_dot))
+	torque = np.cross(m_value,B_body)
+	torque=np.matmul(np.linalg.inv(J),torque.reshape(3,1))
+#	Torque=np.concatenate((Torque,torque.reshape(1,3)),axis=0)
 
+	#torque = np.zeros((1,3)).T
 
 
 
 # Plot Orbit 
 plt.figure()
 ax=plt.axes(projection='3d')
-ax.plot3D(r_total[:,0],r_total[:,1],r_total[:,2],'r')
+ax.plot3D(r_total[1:,0],r_total[1:,1],r_total[1:,2],'r')
 ax.axis('equal')
 plt.xlabel('[km]')
 plt.ylabel('[km]')
@@ -90,17 +97,39 @@ ax.set_title('Detumbling Orbit Dynamics')
 plt.show()
 
 
+# plt.figure()
+f,(ax1,ax2,ax3,ax4)=plt.subplots(4,1,sharey=True)
+ax1.plot(Tspan,q_total[1:,0])
+plt.ylabel('q_1')
+plt.xlabel('t')
+ax1.set_title('HST Quaternion Dynamics')
+ax2.plot(Tspan,q_total[1:,1])
+plt.ylabel('q_2')
+plt.xlabel('t')
+ax3.plot(Tspan,q_total[1:,2])
+plt.ylabel('q_3')
+plt.xlabel('t')
+ax4.plot(Tspan,q_total[1:,3])
+plt.ylabel('q_4')
+plt.xlabel('t')
+plt.show()
+
 # Plot Omega
-plt.figure()
+
 f,(ax1,ax2,ax3)=plt.subplots(3,1,sharey=True)
-ax1.plot(tspan,om_total[:,0])
+ax1.plot(om_total[1:,0])
 plt.ylabel('\omega_1')
 plt.xlabel('t')
-ax1.set_title('Angular Velocity wiht Bdot controller')
-ax2.plot(tspan,om_total[:,1])
+ax1.set_title('Angular Velocity with Bdot controller')
+ax2.plot(om_total[1:,1])
 plt.ylabel('\omega_2')
 plt.xlabel('t')
-ax3.plot(tspan,om_total[:,2])
+ax3.plot(om_total[1:,2])
 plt.ylabel('\omega_3')
 plt.xlabel('t')
 plt.show()
+'''
+plt.figure()
+plt.plot(Torque[:,0], 'r--', Torque, Torque[:,1], 'bs')
+plt.show()
+'''
