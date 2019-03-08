7e2e8856d0ea171bc34ff7d9b7e6ebac07acffa8
kevintracy7 <43561408+kevintracy7@users.noreply.github.com>
2019-03-07 18:48:20 -0800

    Update HSTdynamics2.py
    Removed bdot math from inside the function.

--- Python/HSTdynamics2.py
+++ Python/HSTdynamics2.py
@@ -1,4 +1,4 @@
-def HSTdynamics2(init_state, t, mu, J, J_inv, B_eci, m_max, power_max, Sun2Earth):
+def HSTdynamics2(init_state, t, mu, J, J_inv, B_eci, m_max, power_max, Sun2Earth, torque):
 # HSTdynamics: Contains full ODE dynamics for full spacecraft state.
 #
 # Inputs:
@@ -21,6 +21,8 @@ def HSTdynamics2(init_state, t, mu, J, J_inv, B_eci, m_max, power_max, Sun2Earth
     from subroutines import q2rot
     from sunflux import sunflux
     import pdb
+    
+
 
     # Unpack Initial State
     rvec = init_state[:3]
@@ -28,31 +30,22 @@ def HSTdynamics2(init_state, t, mu, J, J_inv, B_eci, m_max, power_max, Sun2Earth
     r = np.linalg.norm(rvec)
     om0 = init_state[6:9]
     q0 = init_state[9:13]
-    torque0 = init_state[13:16]
-    powercon0 = init_state[16:17]
-    powergen0 = init_state[17:]
 
-    # Rotate B_eci into Satellite Body Frame
-    B_body = np.dot(q2rot(q0),B_eci)
-
-    # Compute Torque using B-dot Control Law for Next Epoch
-    B_dot = -np.cross(om0,B_body)                                               # Compute B_dot
-    m_value = -np.multiply(m_max,np.sign(B_dot))#*np.linalg.norm(np.tanh(om0))              # Direction of Magnetic Moment   
-    torque = np.cross(m_value,B_body)                                           # Compute Torque
-    torque = torque*(abs(om0)>3*np.pi/180)                                      # Turn off Torque within Omega Limits 
     
-    powercon = np.sum(np.dot(np.square(m_value/m_max),power_max))      
-    powergen = sunflux(np.transpose(rvec), Sun2Earth, np.transpose(q0))
+    q0 /= np.linalg.norm(q0)
+    
+
  
     # matrix linear equation
     rv_dot = np.dot(np.block([[np.zeros((3,3)),      np.eye(3)],
                    [-(mu/r**3)*np.eye(3), np.zeros((3,3))]]), init_state[:6])
     om_dot = np.dot(J_inv, -np.cross(om0,np.dot(J,om0))+torque)
+
     q_dot = qkin(q0, om0)
-    torque_dot = torque - torque0
-    powercon_dot = powercon - powercon0
-    powergen_dot = powergen - powergen0
-    x_dot = np.concatenate((rv_dot, om_dot, q_dot, torque_dot, powercon_dot, powergen_dot))
+
+    x_dot = np.concatenate((rv_dot, om_dot, q_dot))
+
+
     return x_dot
 
 
